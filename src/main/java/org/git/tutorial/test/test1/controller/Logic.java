2edd78aadf2f67415fbacc7a0b61e8c4f6f79f9a
parag <parag.phatowali@timesgroup.com>
2016-07-08 14:44:52 +0530

    logic

---
+++ src/main/java/org/git/tutorial/test/test1/controller/Logic.java
@@ -0,0 +1,14 @@
+package org.git.tutorial.test.test1.controller;
+
+public class Logic {
+	
+	public void logic1(){
+		for(int a = 10; a >0 ; a--){
+			System.err.println();
+			for(int b = 1 ; b < a ; b++){
+				System.err.print(" * ");
+			}
+		}
+	}
+
+}

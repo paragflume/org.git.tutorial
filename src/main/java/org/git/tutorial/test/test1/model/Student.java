d3b0bd2f6ec083a27681b2b33d74a231072145e1
parag <parag.phatowali@timesgroup.com>
2016-07-09 12:53:48 +0530

    change in Student.java file

--- src/main/java/org/git/tutorial/test/test1/model/Student.java
+++ src/main/java/org/git/tutorial/test/test1/model/Student.java
@@ -1,31 +1,42 @@
 package org.git.tutorial.test.test1.model;
 
-public class Student{
+public class Student {
 
-    private String name;
-    private String clas;
-    private int roleNo = 0;
-    
-    public void setName(String name){
-      this.name = name;
-    }
-    
-    public void setClaz(String clas){
-      this.clas = clas;
-    }
-    
-    public String getName(){
-      return name;
-    } 
-    public String getClas(){
-      return clas;
-    }
+	private String name;
+	private String clas;
+	private int roleNo = 0;
+	private char section;
 
-	public void setRollNo(int rollNo){
-		this.roleNo = rollNo;
+	public String getName() {
+		return name;
 	}
-	public int getRollNo(){
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getClas() {
+		return clas;
+	}
+
+	public void setClas(String clas) {
+		this.clas = clas;
+	}
+
+	public int getRoleNo() {
 		return roleNo;
+	}
+
+	public void setRoleNo(int roleNo) {
+		this.roleNo = roleNo;
+	}
+
+	public char getSection() {
+		return section;
+	}
+
+	public void setSection(char section) {
+		this.section = section;
 	}
 
 }

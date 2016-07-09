6d5034fac198339bdbb8ade66eb09f9e43693f8c
parag <parag.phatowali@timesgroup.com>
2016-07-09 11:58:27 +0530

    ready to merge

--- src/main/java/org/git/tutorial/test/test1/controller/HomeController.java
+++ src/main/java/org/git/tutorial/test/test1/controller/HomeController.java
@@ -11,12 +11,6 @@ public class HomeController {
 
 	private Integer counter = 0;
 	
-<<<<<<< HEAD
-	@RequestMapping(value = { "admin/incrementpage.html" }, method = { RequestMethod.GET })
-	public String incrementPage(HttpServletRequest req) {
-		counter++;
-		return "pages/increment";
-=======
 	@RequestMapping(value = { "admin/homepage.html" }, method = { RequestMethod.GET })
 	public String adminPage(HttpServletRequest req) {
 		return "adminpage";
@@ -25,25 +19,12 @@ public class HomeController {
 	@RequestMapping(value = { "/home/homepage.html" }, method = { RequestMethod.GET })
 	public String homePage(HttpServletRequest req) {
 		return "home/homepage";
->>>>>>> refs/remotes/origin/org.git.tutorial
 	}
 	
-<<<<<<< HEAD
-
-	@RequestMapping(value = { "admin/homepage.html" }, method = { RequestMethod.GET })
-	public String adminPage(HttpServletRequest req) {
-		return "adminpage";
-	}
-	
-	@RequestMapping(value = { "admin/profilepage.html" }, method = { RequestMethod.GET })
-	public String profilePage(HttpServletRequest req) {
-		return "admin/profliepage";
-=======
 	@RequestMapping(value = { "admin/incrementpage.html" }, method = { RequestMethod.GET })
 	public String incrementPage(HttpServletRequest req) {
 		counter++;
 		return "pages/increment";
->>>>>>> refs/remotes/origin/org.git.tutorial
 	}
 	
 	@RequestMapping(value = { "admin/decremaentpage.html" }, method = { RequestMethod.GET })
@@ -52,17 +33,10 @@ public class HomeController {
 		req.setAttribute("counter", counter);
 		return "pages/decrement";
 	}
-<<<<<<< HEAD
-	
-	@RequestMapping(value = { "/home/homepage.html" }, method = { RequestMethod.GET })
-	public String homePage(HttpServletRequest req) {
-		return "home/homepage";
-=======
 
 	@RequestMapping(value = { "admin/profilepage.html" }, method = { RequestMethod.GET })
 	public String profilePage(HttpServletRequest req) {
 		return "admin/profliepage";
->>>>>>> refs/remotes/origin/org.git.tutorial
 	}
 
 }

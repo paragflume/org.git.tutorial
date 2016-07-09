dfb8eaeff8c995de227b6e403a53b2397f549a90
parag <parag.phatowali@timesgroup.com>
2016-07-09 13:00:05 +0530

    added gender field

--- src/main/java/org/git/tutorial/test/test1/model/Student.java
+++ src/main/java/org/git/tutorial/test/test1/model/Student.java
@@ -1,24 +1,33 @@
 package org.git.tutorial.test.test1.model;
 
-public class Student{
+public class Student {
 
-    private String name;
-    private String clas;
-    
-    public void setName(String name){
-      this.name = name;
-    }
-    
-    public void setClaz(String clas){
-      this.clas = clas;
-    }
-    
-    public String getName(){
-      return name;
-    } 
-    public String getClas(){
-      return clas;
-    }
+	private String name;
+	private String clas;
+	private char gender;
 
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setClaz(String clas) {
+		this.clas = clas;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getClas() {
+		return clas;
+	}
 
 }

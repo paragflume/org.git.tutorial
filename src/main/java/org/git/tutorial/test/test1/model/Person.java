4d480c3720a0e0940917ab1c94f7e2ab9030acb9
parag <parag.phatowali@timesgroup.com>
2016-07-09 11:35:20 +0530

    add model

---
+++ src/main/java/org/git/tutorial/test/test1/model/Person.java
@@ -0,0 +1,15 @@
+package org.git.tutorial.test.test1.model;
+
+public class Person {
+	private String fname;
+	private String lname;
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+}
